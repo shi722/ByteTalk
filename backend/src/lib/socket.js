6d7531f3e4c99c97f872c2d319eabc67927293b9
Shivanshu_Solanki <122626469+shi722@users.noreply.github.com>
2026-01-14 03:13:59 +0530

    Update socket.js

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -7,11 +7,22 @@ import Message from "../models/message.model.js";
 const app = express();
 const server = http.createServer(app);
 
+// const io = new Server(server, {
+//   cors: {
+//     origin: ["http://localhost:5173" "https://bytetalk-fr.onrender.com"],
+//   },                               
+// });
 const io = new Server(server, {
   cors: {
-    origin: ["https://bytetalk-fr.onrender.com"],
-  },                               //http://localhost:5173
+    origin: [
+      "http://localhost:5173",
+      "https://bytetalk-fr.onrender.com"
+    ],
+    credentials: true
+  }
 });
+
+
 
 export function getReceiverSocketId(userId) {
   return userSocketMap[userId];
